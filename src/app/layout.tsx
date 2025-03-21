aa2845e2cf38fc8b09bcbf66d516e4362854563a
Druky1 <official.spatra@gmail.com>
2025-03-22 00:04:10 +0530

    added auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/app/components/theme-provider";
+import { dark } from '@clerk/themes'
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -23,12 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{baseTheme: dark}}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${geistMono.className} antialiased`}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
