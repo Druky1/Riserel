7031a48af6d6f8fccfabb39e67df66ae166b8af9
Druky1 <official.spatra@gmail.com>
2025-03-22 23:59:41 +0530

    added required tweaks

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,30 +1,48 @@
 import { Button } from "@/components/ui/button";
-import { SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-10 bg-[#0A0A0A]">
-      <nav className="w-full border-b mx-auto border-white/20 py-1.5 px-4">
-        <div className="flex justify-between items-center w-full">
+    <header className="sticky top-0 z-10 bg-[#0A0A0A] mt-8">
+      <div className="max-w-7xl mx-auto flex justify-between items-center w-full border border-muted-foreground/20 py-2 px-4">
+        <div className="flex items-center">
           <Link href={"/"}>
-            <Image src="/Logo.png" width={30} height={30}  alt="logo-image" className="w-[30px] h-[30px]"/>
-          </Link>
-          <nav className="hidden md:flex items-center space-x-12 text-sm tracking-tight">
-            <a href="#" className=" text-white/70 hover:text-white transition">FEATURES</a>
-            <a href="#" className=" text-white/70 hover:text-white transition">MODELS</a>
-            <a href="#" className=" text-white/70 hover:text-white transition">GUIDE</a>
-            <a href="#" className=" text-white/70 hover:text-white transition">FAQ</a>
-          </nav>
-          <SignedOut>
-            <Button asChild className="rounded-none p-2">
-              <Link className="text-sm tracking-tight" href={"/sign-in"}>Get started</Link>
-            </Button>
-          </SignedOut>
+            <Image
+              src="/Logo.png"
+              width={30}
+              height={30}
+              alt="logo-image"
+              className="w-[35px] h-[35px]"
+            />
+          </Link>         
         </div>
-      </nav>
+        <nav className="hidden md:flex items-center justify-center flex-grow space-x-12 text-sm tracking-tight">
+          <a href="#" className="text-white/70 hover:text-white transition">
+            Features
+          </a>
+          <a href="#" className="text-white/70 hover:text-white transition">
+            Models
+          </a>
+          <a href="#" className="text-white/70 hover:text-white transition">
+            Guide
+          </a>
+          <a href="#" className="text-white/70 hover:text-white transition">
+            FAQ
+          </a>
+          <a href="#" className="text-white/70 hover:text-white transition">
+            About
+          </a>
+        </nav>
+          <div className="flex items-center space-x-3">
+            <Button asChild className="bg-white text-black text-sm font-medium rounded-none px-3 py-1">
+              <Link href={"/sign-in"}>
+                Get Started
+              </Link>
+            </Button>
+          </div>
+      </div>
     </header>
   );
 };
